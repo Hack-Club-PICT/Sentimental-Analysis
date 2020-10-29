e11bdbd6696c3bae4f90bd7f9d5dcf5b184fa098
pradyumnap <pradyumnaraje.patil@gmail.com>
2020-10-29 20:19:32 +0530

    Deployed Backend

--- backend/sentimentalbackend/settings.py
+++ backend/sentimentalbackend/settings.py
@@ -20,12 +20,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'dummy'
+SECRET_KEY = 'DUMMY'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['https://peaceful-fortress-35616.herokuapp.com/','127.0.0.1']
+ALLOWED_HOSTS = ['gentle-gorge-50568.herokuapp.com','127.0.0.1']
 
 
 # Application definition
@@ -120,11 +120,9 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
-
-STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
+STATIC_URL = '/static/'
 
-CORS_ORIGIN_WHITELIST = [
-     'http://localhost:3000'
-]
+import django_heroku
+django_heroku.settings(locals())
 
